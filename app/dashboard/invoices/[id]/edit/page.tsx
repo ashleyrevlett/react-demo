9ccc8159b6401cf07cd1d6d754ed525f435606a1
ashley revlett <ashleyrevlett@gmail.com>
2024-02-14 14:30:43 -0500

    Add metadata and page titles

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,6 +2,22 @@ import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
+import { Metadata, ResolvingMetadata } from 'next'
+
+type Props = {
+  params: { id: string }
+}
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // read route params
+  return {
+    title: `Edit Invoice ${params.id}`,
+  }
+}
+
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
