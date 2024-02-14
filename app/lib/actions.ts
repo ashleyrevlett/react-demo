c006b3b14c5375f751db1bb9480368e2d6d73271
ashley revlett <ashleyrevlett@gmail.com>
2024-02-14 11:21:36 -0500

    Add update, delete for invoices

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -16,6 +16,7 @@ const FormSchema = z.object({
 
 // id and date fields aren't passed in form data, but are inserted into db
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
   // coerce and validate formdata types
@@ -38,4 +39,32 @@ export async function createInvoice(formData: FormData) {
 
   // go back to main invoices list after create
   redirect('/dashboard/invoices');
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  // @TODO add permissions check
+
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+  // @TODO add error handling
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+  // @TODO add permissions check
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
 }
